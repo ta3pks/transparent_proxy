5d32d8dc1a3890a132daf27fc515f5a8eff71fd0
nikos efthias <nikos@mugsoft.io>
2022-06-10 21:23:43 +0300

    copy buffers

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,9 @@
-use std::error::Error;
+use std::{error::Error, io::ErrorKind};
+
+use tokio::{
+	io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+	net::TcpStream,
+};
 
 use crate::Flags;
 
@@ -14,6 +19,23 @@ pub async fn handle_client(
 			flags.username.unwrap_or_default(),
 			flags.password.unwrap_or_default()
 		))
+	);
+	let mut server = TcpStream::connect(flags.target_addr).await?;
+	let (mut reader, mut writer) = client.split();
+	let buf_reader = BufReader::new(&mut reader);
+	let mut lines = buf_reader.lines();
+	let first_line = lines.next_line().await?.ok_or(std::io::Error::new(
+		ErrorKind::InvalidData,
+		"missing init data",
+	))?;
+	dbg!(&first_line);
+	server
+		.write_all(format!("{first_line}\r\n{auth_line}\r\n").as_bytes())
+		.await?;
+	let (mut server_reader, mut server_writer) = server.split();
+	let (_, _) = tokio::join!(
+		tokio::io::copy(&mut reader, &mut server_writer),
+		tokio::io::copy(&mut server_reader, &mut writer)
 	);
 	unimplemented!()
 }
